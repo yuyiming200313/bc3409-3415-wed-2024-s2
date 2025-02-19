4683e88294d26ac1c5fedaf6ecf3a150176313b8
yuyiming200313 <yuyiming200313@gmail.com>
2025-02-19 08:35:35 +0000

    1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template, request
+import textblob
 
 app = Flask(__name__)
 
@@ -15,6 +16,16 @@ def prediction():
 def predicted_DBS():
     q = float(request.form.get("q"))
     return(render_template("predicted_DBS.html", r=(-50.6*q)+90.2))
+
+@app.route("/text_sentiment", methods=["GET","POST"])
+def text_sentiment():
+    return(render_template("text_sentiment.html"))
+
+@app.route("/sentiment_result", methods=["GET","POST"])
+def sentiment_result():
+    q = request.form.get("q")
+    r = textblob.TextBlob(q).sentiment
+    return(render_template("sentiment_result.html", r=r))
 
 if __name__ == "__main__":
     app.run()
